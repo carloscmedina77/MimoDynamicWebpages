53acbdf8d7ab0ea7d99a8b2ec5496e6c696226a6
Carlos Medina <CARLOSCMEDINA@YAHOO.COM>
2023-01-28 13:13:42 -0600

    Add files via upload

---
+++ mimo28.js
@@ -0,0 +1,97 @@
+// const miles = [33, 95, 79];
+// let total = 0;
+// for (const element of miles) {
+
+//     total
+//         += element;
+// }
+// console.log(total);
+
+// const miles = [33, 95, 79];
+// const total =
+//     miles
+//         .
+//         reduce
+//         (
+//     )
+//     ;
+
+// const miles = [33, 95, 79];
+// const total = miles.reduce(
+//     function
+//         (
+//     ) {
+//     }
+// );
+
+// const miles = [33, 95, 79];
+// const total = miles.reduce(function (
+//     prev,
+
+//     curr
+// ) { });
+
+// const miles = [33, 95, 79];
+// const total = miles.reduce(function (prev, curr) {
+
+//     return
+
+//     prev + curr
+//         ;
+// });
+// console.log(total);
+
+// const miles = [33, 95, 79];
+// const total = miles.reduce(
+//     (prevVal, currVal)
+
+//         =>
+
+//         prevVal + currVal
+// );
+// console.log(total);
+
+// const prices = [17.50, 20.25, 24.75, 13.50, 8.50];
+// const discountFactor = [0.9, 0.9, 1.0, 1.0, 0.9];
+// const total = prices.reduce(function (prevVal, currVal,
+//     index
+// ) {
+//     return prevVal + (currVal * discountFactor[
+//         index
+//     ]);
+// });
+// console.log(total);
+
+// const prices = [17.50, 20.25, 24.75, 13.50, 8.50];
+// const total =
+//     prices
+//         .
+//         reduce
+//         (function (prevVal, currVal) {
+//             return prevVal + currVal;
+//         });
+// console.log(total);
+
+// const values = [4, 3, 2, 1];
+// const factorial = values.reduce(function (
+//     prev
+//     , curr) {
+//     return prev * curr;
+// });
+// console.log(factorial);
+
+
+// const words = ["The", "house", "is", "red"];
+// const sentence = words.reduce(function (prev, curr) {
+
+//     return
+//     prev + " " + curr;
+// });
+// console.log(sentence);
+
+// const prices = [17.50, 20.25, 24.75, 13.50, 8.50];
+// const total = prices.reduce((prevVal, currVal) => prevVal
+//     + currVal
+// );
+// console.log(total);
+
